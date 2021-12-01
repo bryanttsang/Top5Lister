13d7d9b93a05aa205545d969b0f1be290cd438b1
Bryant Tsang <wtsang1931@bths.edu>
2021-12-01 16:34:48 -0500

    designed navigation bar

--- client/src/components/HomeScreen.js
+++ client/src/components/HomeScreen.js
@@ -1,9 +1,15 @@
 import React, { useContext, useEffect } from 'react'
 import { GlobalStoreContext } from '../store'
 import ListCard from './ListCard.js'
-import { Fab, Typography } from '@mui/material'
+import { Button, Fab, Typography } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add';
 import List from '@mui/material/List';
+import SortIcon from '@mui/icons-material/Sort';
+import {Box, TextField, Grid} from '@mui/material';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import FunctionsOutlinedIcon from '@mui/icons-material/FunctionsOutlined';
 /*
     This React component lists all the top5 lists in the UI.
     
@@ -35,18 +41,39 @@ const HomeScreen = () => {
             }
             </List>;
     }
+
+    const navBar = (
+        <Grid container direction="row" justifyContent="space-around">
+            <Grid item>
+                <Box>
+                    <Button style={{color: 'black'}}> <HomeOutlinedIcon style={{fontSize:'36pt'}}/> </Button>
+                    <Button style={{color: 'black'}}> <GroupsOutlinedIcon style={{fontSize:'36pt'}}/> </Button>
+                    <Button style={{color: 'black'}}> <PersonOutlinedIcon style={{fontSize:'36pt'}}/> </Button>
+                    <Button style={{color: 'black'}}> <FunctionsOutlinedIcon style={{fontSize:'36pt'}}/> </Button>
+                </Box>
+            </Grid>
+            <Grid item>
+                <Box sx={{ width: '250pt', maxWidth:'100%'}}>
+                    <TextField
+                        fullWidth
+                        id="filled-search"
+                        label="Search"
+                        type="search"
+                    />
+                </Box>
+            </Grid>
+            <Grid item>
+                <Button style={{fontSize:'16pt', color: 'black'}} variant="text" endIcon={<SortIcon style={{fontSize:'36pt'}}/>}>SORT BY</Button>
+            </Grid>
+        </Grid>
+    );
+
     return (
         <div id="top5-list-selector">
+            {/* tabs, search, sort */}
             <div id="list-selector-heading">
-            <Fab 
-                color="primary" 
-                aria-label="add"
-                id="add-list-button"
-                onClick={handleCreateNewList}
-            >
-                <AddIcon />
-            </Fab>
-                <Typography variant="h2">Your Lists</Typography>
+                {navBar}
+                {/* tabs, search, sort */}
             </div>
             <div id="list-selector-list">
                 {
--- client/src/components/Statusbar.js
+++ client/src/components/Statusbar.js
@@ -1,7 +1,8 @@
 import { useContext } from 'react'
 import AuthContext from '../auth';
 import { GlobalStoreContext } from '../store'
-import { Typography } from '@mui/material'
+import { IconButton, Typography } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add';
 
 /*
     Our Status bar React component goes at the bottom of our UI.
@@ -12,12 +13,28 @@ function Statusbar() {
     const { auth } = useContext(AuthContext);
     const { store } = useContext(GlobalStoreContext);
 
+    function handleCreateNewList() {
+        store.createNewList();
+    }
+
     if (auth.loggedIn)
-        return (
-            <div id="top5-statusbar">
-                <Typography variant="h4">{store.currentList ? "Top 5 " + store.currentList.name : ""}</Typography>
+    return (
+        // <div id="top5-statusbar">
+        //     <Typography variant="h4">{store.currentList ? "Top 5 " + store.currentList.name : ""}</Typography>
+        // </div>
+        <div id="top5-statusbar">
+            <div id="top5-statusbar-text">
+                <IconButton 
+                    aria-label="add"
+                    id="add-list-button"
+                    onClick={handleCreateNewList}
+                >
+                    <AddIcon style={{fontSize:'48pt', color:'#000000'}} />
+                </IconButton>
+                <Typography variant="h2">Your Lists</Typography>
             </div>
-        );
+        </div>
+    );
     return <></>;
 }
 
